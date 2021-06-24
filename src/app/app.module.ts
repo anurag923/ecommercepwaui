e55a2cb714b83d93797b39e4e7c1b913b23bea04
Anurag Pati <anuraganalogit@gmail.com>
2021-06-24 18:00:00 +0530

    anurag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './config/header/header.component';
+import { FooterComponent } from './config/footer/footer.component';
+import { SidebarComponent } from './config/sidebar/sidebar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
